a781f1e0f2e4ae7707320faa34d11b872cc03f36
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-08-22 12:43:55 -0300

    feat: Create Smartphone class attributes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; }
+        protected string IMEI { get; }
+        protected int Memoria { get; set; }
 
         public Smartphone(string numero)
         {
